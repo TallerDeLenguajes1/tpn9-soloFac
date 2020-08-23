5e91add5ac9be7c69d824bf78b2625fd09887cbe
soloFac <francopdlr@gmail.com>
2020-08-23 16:52:52 -0300

    añado punto 1

---
+++ CodigoMorse/Clases.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Encodings;
+
+namespace Helpers
+{
+    public class SoporteParaConfiguracion
+    {
+        static public void CrearArchivoDeConfiguracion(string ruta, string nombre)    //Crea archivos con extension .dat
+        {
+            string RutaArchivo = ruta + nombre + ".dat";
+
+            if (Directory.Exists(ruta) == false) Directory.CreateDirectory(ruta);
+
+            if (!File.Exists(RutaArchivo))
+            {
+                FileStream MiArchivo = new FileStream(RutaArchivo, FileMode.Create);      //Crea archivo destino.dat
+                MiArchivo.Close();
+            }
+        }
+
+        static public void LeerConfiguracion(string RutaArchivo)
+        {
+            using (FileStream MiArchivo = new FileStream(RutaArchivo, FileMode.Open))   //Especialmente para archivos binarios "FileStream"
+            {
+                //Devuelve el puntero a 0, para leer el archivo desde el inicio
+                using (StreamReader MiStream = new StreamReader(MiArchivo))     //Utilizo StreamReader para leer el archivo de texto de Configuracion que es binario
+                {
+                    //Restablezco la posicion del puntero del archivo al comienzo
+                    MiArchivo.Position = 0;
+                    //Leyendo el archivo desde StreamReader - obtiene texto a partir de archivos binarios legibles en este formato
+                    string cadena = MiStream.ReadToEnd();
+                    Console.WriteLine("La cadena leida con StreamReader es: " + cadena);
+
+                    //Forma de leer a partir de FileStream
+                    //byte[] buffer = new byte[128];
+                    //MiArchivo.Read(buffer, 0, 128);
+                    //string cadena2 = System.Text.Encoding.Default.GetString(buffer);    //Convierte los bytes a string
+                    //Console.WriteLine("La candena leida con FileStream es: " + cadena2);
+                }
+            }
+        }
+
+        
+
+
+    }
+}
---
+++ CodigoMorse/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+
+using Helpers;
+
+
+namespace CodigoMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PUNTO 1
+            string ruta = @"C:\Users\Franco\Desktop\Repositorios\codebin#\TPN9\archivobin\temp\";    //Recordar siempre poner la ultima barra
+            string nombre = "destino";
+            string RutaArchivo = ruta + nombre + ".dat";
+
+            //SoporteParaConfiguracion.CrearArchivoDeConfiguracion(ruta, nombre);
+
+            //SoporteParaConfiguracion.LeerConfiguracion(RutaArchivo);
+
+            //PUNTO 2
+
+            string RutaPListar = @"C:\Users\Franco\Desktop\Repositorios\codebin#\TPN9\CodigoMorse\bin\Debug\";
+
+            //Creo una instancia de DirectoryInfo para poder listar la informacion y usar metodos de FileInfo
+            DirectoryInfo directorio = new DirectoryInfo(RutaPListar);
+            FileInfo[] Files = directorio.GetFiles();
+
+            string Origen;
+            //Para cada Archivo dentro del directorio para listar
+            foreach (FileInfo Archivos in Files)
+            {
+                //Obtengo el nombre de cada archivo y lo muestro por pantalla
+                string NombreArchivo = Path.GetFileName(Archivos.ToString()).ToString();
+                Console.WriteLine(NombreArchivo + "\n");
+
+                //Obtengo la extension y la comparo, si coincide muevo el archivo a ruta
+                string extension = Path.GetExtension(Archivos.ToString()).ToString();
+                if (extension == ".txt" || extension == ".mp3")
+                {
+                    Origen = Archivos.ToString();
+                    File.Move(Origen, ruta + NombreArchivo);
+                }
+            }
+
+        }
+    }
+}
